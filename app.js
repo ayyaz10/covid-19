88aba5b7635f07a2e8b263f2b38ac81ff086612a
codementee <codementee@outlook.com>
2020-04-04 20:58:54 +0500

    update

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const xhr = new XMLHttpRequest();
 
+// fetching confirmed, recovered and total deaths
 xhr.open('GET', 'https://covid19.mathdro.id/api', true);
 
 xhr.onload = ()=>{
     const response = JSON.parse(xhr.responseText);
-    document.getElementsByClassName('confirmed-cases')[0].innerHTML = response.confirmed.value;
-    document.getElementsByClassName('recovered-cases')[0].innerHTML = response.recovered.value;
-    document.getElementsByClassName('death-cases')[0].innerHTML = response.deaths.value;
+    const totalConfirmed = document.getElementsByClassName('confirmed-cases')[0].innerHTML = response.confirmed.value;
+    const totalRecovered = document.getElementsByClassName('recovered-cases')[0].innerHTML = response.recovered.value;
+    const totalDeaths = document.getElementsByClassName('death-cases')[0].innerHTML = response.deaths.value;
+    document.getElementsByClassName('active-cases')[0].innerHTML = totalConfirmed - (totalRecovered + totalDeaths);
+    document.getElementsByClassName('closed-cases')[0].innerHTML = totalRecovered + totalDeaths;
 }
 
 xhr.send();
