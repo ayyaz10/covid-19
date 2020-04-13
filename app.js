ff7dd565ec5eb424e8a1c6c678226c5b66b84c33
codementee <codementee@outlook.com>
2020-04-13 19:12:17 +0500

    update

--- app.js
+++ app.js
@@ -17,15 +17,19 @@ function fetchApiData(){
             return response.json();
         })
         .then(data => {
+            function countriesCount(){
+                let i;
+            for(i = 0; i < data.length - 2; i++){
+                
+            }
+            return i;
+        }
+        document.querySelector('.countries-count').textContent = countriesCount();
             populateCountryData(data);
         })
-        // .catch((e) => {
-        //     // console.log(`opps ${e}`)
-        // })
     }
 
-    function populateCountryData(data)
-        {
+    function populateCountryData(data){
         function sortByCases(data){
         data.sort(function(a, b){
             return b.cases - a.cases;
@@ -45,8 +49,7 @@ function fetchApiData(){
                 return input;
             } 
              else {
-                input += "+";
-                return input;
+                return `+${input}`;
             }
         }
 
@@ -70,7 +73,8 @@ function fetchApiData(){
         const casesTested = removeZero(covid.tests)
         const perOneMillionTestedCases = removeZero(covid.testsPerOneMillion)
 
-
+        // injecting data to the table
+        
         // function that creates td's
         function createDataCell(cellData){
             const td = document.createElement('td');
@@ -79,8 +83,8 @@ function fetchApiData(){
             return tr;
         }
 
-
-        createDataCell(covid.country);
+        
+        const countries = createDataCell(covid.country).children[0];
         createDataCell(covid.cases);
         const newCases = createDataCell(todayCases).children[2];
         createDataCell(peopleDied);
@@ -95,6 +99,7 @@ function fetchApiData(){
 
         tBody.appendChild(tr);
 
+        // styling new cases and new deaths cells + styling international conveyances to distinguish them from other countries.
         if(newCases.textContent.includes('+')){
             newCases.style.background = "#f4f9a7";
             // ff0000
@@ -102,132 +107,12 @@ function fetchApiData(){
         if( newDeaths.textContent.includes('+')){
             newDeaths.style.background = "#ff0000";
             newDeaths.style.color = "#ffffff";
+        } else if (covid.country.toLowerCase().includes('diamond princess') || covid.country.toLowerCase().includes('ms zaandam')){
+            countries.style.color= "blue";
+            countries.style.fontStyle = "italic";
         }
     });  
 }  
 
-
+// calling main function
 document.addEventListener('DOMContentLoaded', () => { fetchApiData(); });
-
-
-          // switch(response.countryRegion) {
-
-            // }
-        //     // let usDeaths = 0;
-        //     let usConfirmed = 0;
-        //     let franceConfirmed = 0;
-        //     let thatCconfirmed = 0;
-        //     let output = '';
-        //     let array = "";
-        //     const result = response.map((data)=>{
-        //         array = data.countryRegion;
-        //         return array;
-        //     })
-        //     response.forEach((res, i)=>{
-        //         if(res.countryRegion.toLowerCase() === "result") {
-        //             // usConfirmed = usConfirmed + res.confirmed;  
-        //             // usRecovered = usRecovered + res.recovered;  
-        //             usConfirmed = usConfirmed + res.confirmed;  
-
-                   
-
-
-        //         } else if(res.countryRegion.toLowerCase()=== "result"){
-        //             thatCconfirmed += thatCconfirmed +res.confirmed;
-                
-        //     }
-        //          else {
-        //             output += 
-        //             `<tr>
-        //                 <td>${res.countryRegion}</td>
-        //                 <td>${res.confirmed}</td>
-        //                 </br>
-        //             </tr>
-        //             <tr>
-        //             </tr>
-        //             `
-        //         }
-        // })
-        // output += 
-        // `<tr>
-        //     <td>US</td>
-        //     <td>${usConfirmed}</td>
-        //     </br>
-        // </tr>
-        // <tr>
-        //     <td>France</td>
-
-        //     </br>
-        // </tr>
-        // `
-    
-            //   document.querySelector('.fetch-content').innerHTML = output;
-
-    //    fetch(`${data.recovered.detail}`).then((recoveredRes)=>{
-    //     return recoveredRes.json();
-    //     }).then((recoveredRes) => {
-    //         console.log(recoveredRes)
-    //     })
-
-
-
-
-    // function sortTableByColumn(table, column, asc = "true"){
-    //     const dirModifier = asc ? 1 : -1;
-    //     const tBody = table.tBodies [0];
-    //     const rows = Array.from(tBody.querySelectorAll('tr'))
-    //     console.log(rows)
-    //     const sortedRow = rows.sort((a, b) => { 
-    //       console.log(a)
-    //       console.log(b)
-    //   })   
-    //    sortTableByColumn(document.querySelector("table"), 1)   
-    // }
-
-
-        // output +=
-        //     `
-        //     <tr>
-        //         <td>${covid.country}</td>
-        //         <td>${covid.cases}</td>
-        //         <td>${todayCases}</td>
-        //         <td>${covid.deaths}</td>
-        //         <td>${todayDeaths}</td>
-        //         <td>${covid.recovered}</td>
-        //         <td>${covid.active}</td>
-        //         <td>${covid.critical}</td>
-        //         <td>${covid.casesPerOneMillion}</td>
-        //         <td>${covid.deathsPerOneMillion}</td>
-        //     </tr>
-
-        //     `
-
-       //   document.querySelector('.fetch-content').innerHTML = output;
-
-               // console.log(createTd(covid.country))
-        // const countriesCell = createTd(covid.country);
-        // console.log(countriesCell)
-        
-
-        // const countriesCell = createDataCell(covid.country);
-        // const totalCasesCell = createDataCell(covid.country);
-        // const todayCasesCell = createDataCell(covid.country);
-        // const totalDeathsCell = createDataCell(covid.country);
-        // const todayDeathsCell = createDataCell(covid.country);
-        // const totalRecoveredCell = createDataCell(covid.country);
-        // const activeCell = createDataCell(covid.country);
-        // const criticalCell = createDataCell(covid.country);
-        // const casesPerMillionCell = createDataCell(covid.country);
-        // const deathsPerMillionCell = createDataCell(covid.country);
-
-        // tr.appendChild(countriesCell)
-        
-        // tr.appendChild(totalCasesCell)
-        // tr.appendChild(todayCasesCell)
-        // tr.appendChild(totalDeathsCell)
-        // tr.appendChild(todayDeathsCell)
-        // tr.appendChild(totalRecoveredCell)
-        // tr.appendChild(activeCell)
-        // tr.appendChild(criticalCell)
-        // tr.appendChild(casesPerMillionCell)
-        // tr.appendChild(deathsPerMillionCell)
